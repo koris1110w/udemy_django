368d70c4af0e295461ec159215182048b3aa2f0b
koris1110w <koris1110w@gmail.com>
2024-04-09 22:35:39 +0900

    docker対応wip

--- helloworld/helloworldproject/helloworldproject/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-def helloworldfunction(request):
-    return HttpResponse('<h1>HelloWorld!</h1>')
-
-class helloworldClass(TemplateView):
-    template_name = 'hello.html'
